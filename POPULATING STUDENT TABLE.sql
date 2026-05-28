5ed09e9c04ec3b8688d559c5e579d7ce8e7a90da
Blaze Ads <164090214+blazeads@users.noreply.github.com>
2026-05-28 10:02:15 +0200

    Add files via upload

---
+++ POPULATING STUDENT TABLE.sql
@@ -0,0 +1,5 @@
+INSERT INTO students(student_name, student_name, Cellphone, Email)
+VALUES
+('John','Nkomo',0871233456,'John@nkomo.com'),
+('Jake','Green',0671433456,'Jake@green.com'),
+('Alice','Brown',0871983456,'Alice@gmail.com');
